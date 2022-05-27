b6656fbebac73118d997b08073931779b0776aaa
420PAZZU <93506236+420PAZZU@users.noreply.github.com>
2022-05-27 08:09:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,7 +40,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/SHProSearchbot')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/PS_MOVIES1')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
